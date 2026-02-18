2198f7dcc2aeb6f68cfdcce2c41d26d15a316c3a
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2026-02-18 18:32:40 +0100

    Refactor import statements in main.py for clarity and organization

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-from supabase import create_client, Client
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from supabase import create_client, Client
 from bs4 import BeautifulSoup
 import paho.mqtt.client as mqtt
 import re
